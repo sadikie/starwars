b2c9c4dd2767b42d263e69d9d1c80f6f05d08ec0
sadikie williams <sadikiewilliams@sadikies-MacBook-Pro.local>
2017-06-02 10:40:47 -0400

    making an api call from a controller with  service

--- app/components/warslist.component.js
+++ app/components/warslist.component.js
@@ -1,3 +1,21 @@
 appModule.component('warsList', {
-    templateUrl: 'app/templates/warslist.html'
+    templateUrl: 'app/templates/warslist.html',
+    controller: function($http, $scope) {
+        // Simple GET request example:
+        $http({
+            method: 'GET',
+            url: 'https://swapi.co/api/people/',
+        }).then(function successCallback(response) {
+            console.log(response)
+            console.log(response.data.results)
+            $scope.warriors = response.data.results
+                // angular.forEach(warriors, function(warrior){
+                //     $scope.warrior = warrior
+                // })
+        }, function errorCallback(response) {
+            console.log(response)
+        });
+
+
+    }
 })
